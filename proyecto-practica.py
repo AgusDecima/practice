e55368e4cd382ae33ad4810e433e7697dad93201
Agus <jeronimolink360@gmail.com>
2022-06-09 00:43:07 -0300

    mas texto

--- proyecto-practica.py
+++ proyecto-practica.py
@@ -1,6 +1,33 @@
-practica = "holiwis"
+# - Los comentarios se escriben con "#"
 
-print(practica)
+#--------------------------------------
 
-"holiii"
+# - Las cadenas son conjuntos de caraceteres (string)
+
+cadena = "holiwis"
+
+print(cadena)
+print(type(cadena))
+
+#--------------------------------------
+
+# - Un número entero es un tipo de variable "int"
+
+numero = 85
+
+print(type(numero))
+
+#--------------------------------------
+#  IF Y ELSE:
+
+variable = 20
+
+if variable < 2:
+    print("verdadero")
+elif variable == 20:
+    print("la variable es 20")
+else:
+    print("falso")
+
+# el comando "elif" sólo se activará si no se cumplen los requisitos del "if" o el anterior "elif"
 
